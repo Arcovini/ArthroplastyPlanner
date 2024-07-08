4e4fd92738bad633370f6fbc0a74db87e950cfa8
arcovinicius <vinicius.p.arcoverde@gmail.com>
2024-07-08 18:31:48 -0300

    Baseplate Model and Visual Improvements

--- Assets/Scripts/ModelController.cs
+++ Assets/Scripts/ModelController.cs
@@ -8,11 +8,11 @@ namespace AP
     {
         public static GameObject model = null;
 
-        public const float translationStepSize = 5.0f;
-        public const float rotationStepSize = 30.0f;
+        public const float translationStepSize = 0.1f;
+        public const float rotationStepSize = 1.0f;
 
-        public const float fixedTranslationStepSize = 0.5f;
-        public const float fixedRotationStepSize = 3.0f;
+        public const float fixedTranslationStepSize = 1.0f;
+        public const float fixedRotationStepSize = 1.0f;
 
         public static void Translate(Vector3 dir) => model.transform.Translate(dir * translationStepSize * Time.deltaTime, Space.World);
         public static void Rotate(Vector3 axis) => model.transform.Rotate(axis * rotationStepSize * Time.deltaTime, Space.Self);
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -8,8 +8,11 @@ namespace AP
 {
     using MC = ModelController;
 
+
     public class UIManager : MonoBehaviour
     {
+
+
         public class UIButtonAction
         {
             public Action Clicked = null;
@@ -23,14 +26,28 @@ namespace AP
         }
         
         private float timer = 0.0f;
-        private float threshold = 0.15f;
+        private float threshold = 0.40f;
         private bool isButtonPressed = false;
 
+        //variables of the labels
+        float pitchDistance = 0;
+        private Label labelPitchDistance = null;
+        float yawDistance = 0;
+        private Label labelYawDistance = null;
+        float RollDistance = 0;
+        private Label labelRollDistance = null;
         // TODO: refactor
         private void OnEnable()
         {
             UIDocument document = GameObject.Find("UIDocument").GetComponent<UIDocument>();
             VisualElement root = document.rootVisualElement;
+
+
+
+            labelPitchDistance = root.Q<Label>("labelPitchDistance");
+            labelYawDistance = root.Q<Label>("labelYawDistance");
+            labelRollDistance = root.Q<Label>("labelRollDistance");
+
 
             // Rotation
             Button buttonPitchLeft = root.Q<Button>("buttonPitchLeft");
@@ -40,12 +57,13 @@ namespace AP
             Button buttonRollLeft = root.Q<Button>("buttonRollLeft");
             Button buttonRollRight = root.Q<Button>("buttonRollRight");
 
-            buttonPitchLeft. RegisterCallback<MouseDownEvent, UIButtonAction>(OnButtonPressed, new UIButtonAction(()=>MC.FixedStepRotate(Vector3.left),    ()=>MC.Rotate(Vector3.left)),    TrickleDown.TrickleDown);
-            buttonPitchRight.RegisterCallback<MouseDownEvent, UIButtonAction>(OnButtonPressed, new UIButtonAction(()=>MC.FixedStepRotate(Vector3.right),   ()=>MC.Rotate(Vector3.right)),   TrickleDown.TrickleDown);
-            buttonYawLeft.   RegisterCallback<MouseDownEvent, UIButtonAction>(OnButtonPressed, new UIButtonAction(()=>MC.FixedStepRotate(Vector3.down),    ()=>MC.Rotate(Vector3.down)),    TrickleDown.TrickleDown);
-            buttonYawRight.  RegisterCallback<MouseDownEvent, UIButtonAction>(OnButtonPressed, new UIButtonAction(()=>MC.FixedStepRotate(Vector3.up),      ()=>MC.Rotate(Vector3.up)),      TrickleDown.TrickleDown);
-            buttonRollLeft.  RegisterCallback<MouseDownEvent, UIButtonAction>(OnButtonPressed, new UIButtonAction(()=>MC.FixedStepRotate(Vector3.forward), ()=>MC.Rotate(Vector3.forward)), TrickleDown.TrickleDown);
-            buttonRollRight. RegisterCallback<MouseDownEvent, UIButtonAction>(OnButtonPressed, new UIButtonAction(()=>MC.FixedStepRotate(Vector3.back),    ()=>MC.Rotate(Vector3.back)),    TrickleDown.TrickleDown);
+            //buttonPitchLeft. RegisterCallback<MouseDownEvent, UIButtonAction>(OnButtonPressed, new UIButtonAction(()=>MC.FixedStepRotate(Vector3.left),    ()=>MC.Rotate(Vector3.left)),    TrickleDown.TrickleDown);
+            buttonPitchLeft.RegisterCallback<MouseDownEvent, UIButtonAction>(OnButtonPressed, new UIButtonAction(() => { MC.FixedStepRotate(Vector3.left); pitchUpdateEvent(true, 1f) ; }, () => { MC.Rotate(Vector3.left);  pitchUpdateEvent(true, 0.01f);}), TrickleDown.TrickleDown);
+            buttonPitchRight.RegisterCallback<MouseDownEvent, UIButtonAction>(OnButtonPressed, new UIButtonAction(()=> { MC.FixedStepRotate(Vector3.right); pitchUpdateEvent(false,1f); },   ()=> { MC.Rotate(Vector3.right); pitchUpdateEvent(false,01f); }),   TrickleDown.TrickleDown);
+            buttonYawLeft.   RegisterCallback<MouseDownEvent, UIButtonAction>(OnButtonPressed, new UIButtonAction(()=> { MC.FixedStepRotate(Vector3.down); yawUpdateEvent(true, 1f); },    ()=>{ MC.Rotate(Vector3.down); yawUpdateEvent(true, 0.01f); }),    TrickleDown.TrickleDown);
+            buttonYawRight.  RegisterCallback<MouseDownEvent, UIButtonAction>(OnButtonPressed, new UIButtonAction(()=> { MC.FixedStepRotate(Vector3.up); yawUpdateEvent(false, 1f); },      ()=>{ MC.Rotate(Vector3.up); yawUpdateEvent(false, 0.01f); }),      TrickleDown.TrickleDown);
+            buttonRollLeft.  RegisterCallback<MouseDownEvent, UIButtonAction>(OnButtonPressed, new UIButtonAction(()=> { MC.FixedStepRotate(Vector3.forward); RollUpdateEvent(true, 1f);}, ()=>{ MC.Rotate(Vector3.forward); RollUpdateEvent(true, 0.01f); }), TrickleDown.TrickleDown);
+            buttonRollRight. RegisterCallback<MouseDownEvent, UIButtonAction>(OnButtonPressed, new UIButtonAction(()=> { MC.FixedStepRotate(Vector3.back); RollUpdateEvent(false, 1f); },    ()=>{ MC.Rotate(Vector3.back); RollUpdateEvent(false, 0.01f); }),    TrickleDown.TrickleDown);
 
             buttonPitchLeft. RegisterCallback<MouseUpEvent>(OnButtonRelease);
             buttonPitchRight.RegisterCallback<MouseUpEvent>(OnButtonRelease);
@@ -75,6 +93,7 @@ namespace AP
             buttonHorizontalRight.RegisterCallback<MouseUpEvent>(OnButtonRelease);
             buttonVerticalUp.     RegisterCallback<MouseUpEvent>(OnButtonRelease);
             buttonVerticalDown.   RegisterCallback<MouseUpEvent>(OnButtonRelease);
+
         }
 
         private void OnButtonPressed(MouseDownEvent e, UIButtonAction action)
@@ -105,5 +124,60 @@ namespace AP
 
             this.timer = 0.0f;
         }
+
+        private void pitchUpdateEvent(bool isLeft, float increaseRatio) {
+            if (isLeft)
+            {
+                pitchDistance -= 1.0f * increaseRatio;
+            }
+            else
+            {
+                pitchDistance += 1.0f * increaseRatio;
+            }
+            Vector3 modelEulerAngles = MC.model.transform.eulerAngles;
+            float continuousPitchAngle = ConvertToContinuousAngle(modelEulerAngles.x);
+            labelPitchDistance.text = ((int)continuousPitchAngle).ToString() + "°";
+        }
+
+        private void yawUpdateEvent(bool isDown, float increaseRatio)
+        {
+            if (isDown)
+            {
+                yawDistance -= 1.0f * increaseRatio;
+            }
+            else
+            {
+                yawDistance += 1.0f * increaseRatio;
+            }
+            Vector3 modelEulerAngles = MC.model.transform.eulerAngles;
+            float continuousyawAngle = ConvertToContinuousAngle(modelEulerAngles.y);
+            labelYawDistance.text = ((int)continuousyawAngle).ToString() + "°";
+        }
+
+        private void RollUpdateEvent(bool isForward, float increaseRatio)
+        {
+            if (isForward)
+            {
+                RollDistance -= 1.0f * increaseRatio;
+            }
+            else
+            {
+                RollDistance += 1.0f * increaseRatio;
+            }
+            Vector3 modelEulerAngles = MC.model.transform.eulerAngles;
+            float continuousRollAngle = ConvertToContinuousAngle(modelEulerAngles.z);
+            labelRollDistance.text = ((int)continuousRollAngle).ToString() + "°";
+        }
+
+        private float ConvertToContinuousAngle(float angle)
+        {
+            angle = angle % 360f; // Ensure the angle is within -360 to 360 degrees
+            if (angle > 180f)
+                angle -= 360f; // Convert 181-360 to -179 to 0
+            else if (angle < -180f)
+                angle += 360f; // Convert -181 to -360 to 179 to 0
+            return angle;
+        }
+
     }
 }
